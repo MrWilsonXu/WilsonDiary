3003a36659ef5f28c0a4748e8880f970ac9145b2
Wilson <hrxspp@126.com>
2018-01-02 13:50:35 +0800

    pod afnetworking

---
+++ WilsonDev/Pods/Target Support Files/Pods-WilsonDev/Pods-WilsonDev-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WilsonDev : NSObject
+@end
+@implementation PodsDummy_Pods_WilsonDev
+@end
---
+++ WilsonDev/Pods/Target Support Files/Pods-WilsonDevTests/Pods-WilsonDevTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WilsonDevTests : NSObject
+@end
+@implementation PodsDummy_Pods_WilsonDevTests
+@end
