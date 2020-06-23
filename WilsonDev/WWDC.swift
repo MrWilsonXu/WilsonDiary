acc2672123eb317a0976d8187d85c04e5ec715a8
A02442 <yongsheng.xu@wholion.com>
2020-06-23 11:45:49 +0800

    feat: WWDC new feature

---
+++ WilsonDev/WWDC.swift
@@ -0,0 +1,28 @@
+//
+//  WWWDC2020.swift
+//  WilsonDev
+//
+//  Created by Wilson on 2020/6/23.
+//  Copyright © 2020 Wilson. All rights reserved.
+//
+
+import UIKit
+
+class WWDC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func newUIKit() {
+        
+    }
+    
+    func newSwiftVersion() {
+        
+    }
+    
+    func newCoreML() {
+        
+    }
+}
